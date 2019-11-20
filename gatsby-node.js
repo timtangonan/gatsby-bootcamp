e41846d291c9b09b86c30f8d6b194d3f12464b4b
Tim Tangonan <trstangonan@gmail.com>
2019-11-20 23:01:00 +0800

    blog page generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,13 @@
 const path = require('path')
 
+// Create slug for each post
+
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
 
     if (node.internal.type === 'MarkdownRemark') {
         const slug = path.basename(node.fileAbsolutePath, '.md')
+        // takes the path, the last part, and removes file extension
         
         
         createNodeField({
@@ -13,4 +16,37 @@ module.exports.onCreateNode = ({ node, actions }) => {
             value: slug
         })
     }
+}
+
+// Create page for blog posts
+module.exports.createPages = async ({ graphql, actions}) => {
+    const { createPage } = actions
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+
+    // 1. get path to template
+    // 2. get markdown data
+    // 3. create new pages
 }
